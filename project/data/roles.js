708a9e113b4aadf59e9b1846f881a94d5d1cf0ff
Tromwey <93409454+Tromwey@users.noreply.github.com>
2021-11-27 19:10:40 -0600

    Project folder outdated

--- project/data/roles.js
+++
@@ -1,7 +0,0 @@
-import { Role } from 'testcafe'
-import { CREDENTIALS, URLS } from "../data/constants"
-import loginPage from '../pages/login-page'
-
-export const STANDARD_USER = Role(URLS.BASE_URL , async() => {
-    await loginPage.submitLoginForm(CREDENTIALS.STANDARD_USER.USERNAME,CREDENTIALS.STANDARD_USER.PASSWORD)
-}, {preserveUrl:true})
--- project/pages/products-page.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from "testcafe"
-
-class ProductsPage {
-    constructor(){
-        this.productsPageTitle = Selector('.title').withText('Products')
-    }
-}
-
-export default new ProductsPage
