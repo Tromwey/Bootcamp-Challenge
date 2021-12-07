c8ed96f25e83785fb33c6d9cc14a4ce0e0934a1b
Tromwey <93409454+Tromwey@users.noreply.github.com>
2021-12-06 23:05:15 -0600

    minor changes

--- FrontEnd/tests/Task.test.js
+++ FrontEnd/tests/Task.test.js
@@ -3,7 +3,7 @@ import { STANDARD_USER } from "../data/roles"
 import  todayPage from "../pages/today-page"
 import inboxPage from "../pages/inbox-page"
 
-fixture ('Today - Tasks Creation feature test')
+fixture ('Task - Tasks Creation feature test')
     .page `${URLS.BASE_URL}`
     .beforeEach( async t => {
         await t.useRole(STANDARD_USER)
@@ -12,17 +12,17 @@ fixture ('Today - Tasks Creation feature test')
         await inboxPage.cleanUp()
     })
 
-test.meta({ feature: 'today', type: 'smoke' })('As a user, I should be able to create a task by providig a name', async () => {
+test.meta({ feature: 'task', type: 'smoke' })('As a user, I should be able to create a task by providig a name', async () => {
     await todayPage.createTasks()
     await inboxPage.assertCreatedTasks(TASK.DEFAULT_NAME, TASK.TODAY, TASK.SINGLE_TASK)
 })
 
-test.meta({ feature: 'today', type: 'smoke' })('As a user, I should be able to create a task selecting tomorow as due date', async () => {
+test.meta({ feature: 'task', type: 'smoke' })('As a user, I should be able to create a task selecting tomorow as due date', async () => {
     await todayPage.createTasks(TASK.SINGLE_TASK,TASK.TOMORROW)
     await inboxPage.assertCreatedTasks(TASK.DEFAULT_NAME, TASK.TOMORROW, TASK.SINGLE_TASK)
 })
 
-test.meta({ feature: 'today' })('As a user, I should be able to create multiple tasks', async () => {
+test.meta({ feature: 'task' })('As a user, I should be able to create multiple tasks', async () => {
     await todayPage.createTasks(TASK.MANY_TASKS)
     await inboxPage.assertCreatedTasks(TASK.DEFAULT_NAME, TASK.TODAY, TASK.MANY_TASKS)
 })
